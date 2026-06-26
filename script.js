e7e1610a156ce995401714d26df2e5a44ace6d7b
Harsh <harsh1111kashyap@gmail.com>
2026-06-26 07:36:29 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let home = document.getElementById("home-page");
+let about = document.getElementById("about-page");
+let project = document.getElementById("project-page");
+let contact = document.getElementById("contact-page");
+
+function homeBut(){
+    home.style.display = "block";
+    about.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+}
+
+function aboutBut(){
+    about.style.display = "block";
+    home.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+}
+
+function projectBut(){
+    project.style.display = "block";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+}
+
+function contactBut(){
+    contact.style.display = "block";
+    about.style.display = "none";
+    home.style.display = "none";
+    project.style.display = "none";
+}
