97f4bb96a11b73a3c36b181cf2c91c5a2a49e214
Harsh <harsh1111kashyap@gmail.com>
2026-06-26 08:02:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let home = document.getElementById("home-page");
 let about = document.getElementById("about-page");
 let project = document.getElementById("project-page");
 let contact = document.getElementById("contact-page");
+let skill = document.getElementById("skill-page");
 
 function homeBut(){
     home.style.display = "block";
     about.style.display = "none";
     project.style.display = "none";
     contact.style.display = "none";
+    skill.style.display = "none";
 }
 
 function aboutBut(){
@@ -15,6 +17,7 @@ function aboutBut(){
     home.style.display = "none";
     project.style.display = "none";
     contact.style.display = "none";
+    skill.style.display = "none";
 }
 
 function projectBut(){
@@ -22,10 +25,20 @@ function projectBut(){
     contact.style.display = "none";
     about.style.display = "none";
     home.style.display = "none";
+    skill.style.display = "none";
 }
 
 function contactBut(){
     contact.style.display = "block";
+    about.style.display = "none";
+    home.style.display = "none";
+    project.style.display = "none";
+    skill.style.display = "none";
+}
+
+function skillBut(){
+    skill.style.display = "block";
+    contact.style.display = "none";
     about.style.display = "none";
     home.style.display = "none";
     project.style.display = "none";
